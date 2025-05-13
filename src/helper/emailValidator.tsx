4af8018d19de36bedcde37fa6a9f542db0d2361b
Ayesh Jayasekara <ejkpac@gmail.com>
2025-05-13 15:19:30 +0930

    Code Refactoring
    - Fixing RegEx Security Issue
    
    Signed-off-by: Ayesh Jayasekara <ejkpac@gmail.com>

--- src/helper/emailValidator.tsx
+++ src/helper/emailValidator.tsx
@@ -1,5 +1,6 @@
 export function emailValidator(email: string) {
-    const re = /\S+@\S+\.\S+/
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
     if (!email) return "Please fill in this field."
     if (!re.test(email)) return 'Please enter a valid email address!'
     return ''

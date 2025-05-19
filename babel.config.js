f78b7ee83770b8452f6dea324469e4ccf3674ccd
Ayesh Jayasekara <ejkpac@gmail.com>
2025-05-20 01:43:30 +0930

    Paper theme configured
    Signed-off-by: Ayesh Jayasekara <ejkpac@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,12 @@ module.exports = function (api) {
             "nativewind/babel"
         ],
 
+        env: {
+            production: {
+                plugins: ['react-native-paper/babel'],
+            },
+        },
+
         plugins: [["module-resolver", {
 
             alias: {
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,35 +6,50 @@ import React from "react";
 import {StatusBar} from "expo-status-bar";
 import {AuthProvider} from "@/utils/authContext";
 
+import {useMaterial3Theme} from '@pchmn/expo-material3-theme';
+import {useColorScheme} from 'react-native';
+import {MD3DarkTheme, MD3LightTheme, PaperProvider} from 'react-native-paper';
+
 export default function RootLayout() {
+
+    const colorScheme = useColorScheme();
+    const {theme} = useMaterial3Theme();
+
+    const paperTheme =
+        colorScheme === 'dark'
+            ? {...MD3DarkTheme, colors: theme.dark}
+            : {...MD3LightTheme, colors: theme.light};
+
     return (
-        <GluestackUIProvider mode="light">
-            <AuthProvider>
-                <StatusBar style="auto"/>
-                <Stack>
-                    <Stack.Screen
-                        name="(protected)"
-                        options={{
-                            headerShown: false,
-                            animation: "none",
-                        }}
-                    />
-                    <Stack.Screen
-                        name="login"
-                        options={{
-                            headerShown: false,
-                            animation: "none",
-                        }}
-                    />
-                    <Stack.Screen
-                        name="register"
-                        options={{
-                            headerShown: false,
-                            animation: "none",
-                        }}
-                    />
-                </Stack>
-            </AuthProvider>
+        <GluestackUIProvider mode="system">
+            <PaperProvider theme={paperTheme}>
+                <AuthProvider>
+                    <StatusBar style="auto"/>
+                    <Stack>
+                        <Stack.Screen
+                            name="(protected)"
+                            options={{
+                                headerShown: false,
+                                animation: "none",
+                            }}
+                        />
+                        <Stack.Screen
+                            name="login"
+                            options={{
+                                headerShown: false,
+                                animation: "none",
+                            }}
+                        />
+                        <Stack.Screen
+                            name="register"
+                            options={{
+                                headerShown: false,
+                                animation: "none",
+                            }}
+                        />
+                    </Stack>
+                </AuthProvider>
+            </PaperProvider>
         </GluestackUIProvider>
     );
 }

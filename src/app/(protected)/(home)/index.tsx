e2173d7d823d37482fe94bcb4c089e040cea3cc2
Ayesh Jayasekara <ejkpac@gmail.com>
2025-05-21 18:42:08 +0930

    New Generic Components Added
    Signed-off-by: Ayesh Jayasekara <ejkpac@gmail.com>

--- src/app/(protected)/(home)/index.tsx
+++ src/app/(protected)/(home)/index.tsx
@@ -1,12 +1,15 @@
-import {Image, StyleSheet, View, Linking} from "react-native";
-import {AppText} from "@/components/AppText";
+import {Dimensions, StyleSheet, View} from "react-native";
 import {Link, useRouter} from "expo-router";
-import {Button} from "@/components/Button";
-import {useContext} from "react";
+import {useContext, useRef, useState} from "react";
 import {AuthContext} from "@/utils/authContext";
 import {useTheme} from 'react-native-paper';
 import AppBackground from "@/components/generic/AppBackground";
 import GeneralButton from "@/components/generic/GeneralButton";
+import CustomSurface from "@/components/generic/CustomSurface";
+import CustomImageCarousal from "@/components/generic/CustomImageCarousal";
+import PagerView from "react-native-pager-view";
+
+const {height} = Dimensions.get("window");
 
 
 export default function IndexScreen() {
@@ -14,15 +17,24 @@ export default function IndexScreen() {
     const canGoBack = router.canGoBack();
     const authState = useContext(AuthContext);
     const theme = useTheme();
+    const styles = makeStyles(theme);
+    const pagerRef = useRef<PagerView>(null);
+    const [page, setPage] = useState(0);
+    const pages = ['https://media.istockphoto.com/id/2185186618/photo/christmas-tree-and-gift-boxes-on-purple-background-new-year-concept.jpg?s=2048x2048&w=is&k=20&c=oxpxOmKrtn0GRQN6L-p53eYuKSyFjzlmGczq55RkOeY=', 'https://media.istockphoto.com/id/2185186642/photo/christmas-tree-and-gift-boxes-on-purple-background-new-year-concept.jpg?s=2048x2048&w=is&k=20&c=5NIzn4z6uWqAD0y5EbM_jJIDdn4PMfyYpjkWebq15Ys='];
+
 
     return (
         <AppBackground>
             <View className="justify-center flex-1 p-4 ">
 
+                <CustomSurface elevation={5} theme={theme} mode={"elevated"} style={styles.surface}>
+                    <CustomImageCarousal initialPage={0} pages={pages} style={styles.image_slider} theme={theme}
+                                         delay={3000} pagerRef={pagerRef} page={page} setPage={setPage}/>
+                </CustomSurface>
 
-                <AppText center>Index Screen</AppText>
                 <Link href="/home-nested" push asChild>
                     <GeneralButton mode="contained" onPressFunction={() => {
+                        // @ts-ignore
                         router.push("home-nested")
                     }} text={"Push to nested"} style={{}}/>
                 </Link>
@@ -43,3 +55,13 @@ export default function IndexScreen() {
 
     );
 }
+
+const makeStyles = (theme) => StyleSheet.create({
+    surface: {
+        width: "100%",
+        height: height * 0.3,
+    },
+    image_slider: {
+        flex: 1,
+    }
+});

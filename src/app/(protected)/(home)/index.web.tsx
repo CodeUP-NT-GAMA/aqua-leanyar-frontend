868cb860ef7f740c9a363dc6667ccbb235f31e94
Ayesh Jayasekara <ejkpac@gmail.com>
2025-05-23 13:18:55 +0930

    Index Page Layout - WEB implementation
    Signed-off-by: Ayesh Jayasekara <ejkpac@gmail.com>

--- src/app/(protected)/(home)/index.web.tsx
+++ src/app/(protected)/(home)/index.web.tsx
@@ -1,47 +1,122 @@
-import {View} from "react-native";
-import {AppText} from "@/components/AppText";
+import {Dimensions, ScrollView, StyleSheet, View} from "react-native";
 import {Link, useRouter} from "expo-router";
-import {Button} from "@/components/Button";
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {AuthContext} from "@/utils/authContext";
-import {Card} from "@/components/ui/card";
-import {useTheme} from 'react-native-paper';
+import {Button, Card, Divider, Text, useTheme} from 'react-native-paper';
 import AppBackground from "@/components/generic/AppBackground";
+import GeneralButton from "@/components/generic/GeneralButton";
+import FontAwesome from "@expo/vector-icons/FontAwesome6";
 
+const {height, width} = Dimensions.get("window");
+const LeftSwimming = props => <FontAwesome name="person-swimming" size={35} color={props.color}/>
+const LeftShopping = props => <FontAwesome name="basket-shopping" size={35} color={props.color}/>
 
 export default function IndexScreen() {
     const router = useRouter();
     const canGoBack = router.canGoBack();
     const authState = useContext(AuthContext);
     const theme = useTheme();
+    const styles = makeStyles(theme);
+
+    const swimming = () => LeftSwimming({color: theme.colors.primary})
+    const shopping = () => LeftShopping({color: theme.colors.primary})
 
     return (
         <AppBackground>
-            <View className="justify-center flex-1 p-4 ">
-                <Card size="lg" variant="elevated" className="m-3">
-                    <AppText center>
-                        Quick Start
-                    </AppText>
-                    <AppText center>Start building your next project in minutes</AppText>
-                </Card>
+            <ScrollView horizontal={false} showsVerticalScrollIndicator={false} style={styles.container}
+                        contentContainerStyle={styles.container}>
+                <View style={[styles.column_style]}>
 
-                <AppText center>Index` Screen</AppText>
-                <Link href="/home-nested" push asChild>
-                    <Button title="Push to /home-nested"/>
-                </Link>
-                {canGoBack ? (
-                    <Button
-                        title="Back"
-                        theme="primary"
-                        onPress={() => {
-                            router.back();
-                        }}
-                    />
-                ) : null}
-                <Button title="Log out!" onPress={authState.logOut} style={{backgroundColor: theme.colors.primary}}/>
-            </View>
+                    <View>
+                        <Card style={styles.activity_card}>
+                            <Card.Title title="Ready to get wet?" subtitle="Let's Go!" left={swimming}
+                                        titleStyle={styles.card_title}/>
+                            <Divider bold={true}/>
+                            <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
+                            <Card.Content>
+                                <Text variant="bodyMedium" style={styles.card_content}>From a puddle of water to giant
+                                    waves, we have got them all!</Text>
+                            </Card.Content>
+                            <Card.Actions>
+                                <Button mode={"contained"}>Tell me more!</Button>
+                            </Card.Actions>
+                        </Card>
+                    </View>
+                    <View>
+                        <Card style={styles.shop_card}>
+                            <Card.Title title="Fancy some merch?" subtitle="All you need to jump right in!"
+                                        left={shopping}
+                                        titleStyle={styles.card_title}/>
+                            <Divider bold={true}/>
+                            <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
+                            <Card.Actions>
+                                <Button mode={"contained"}>Show All</Button>
+                            </Card.Actions>
+                        </Card>
+                    </View>
+
+                    <View>
+                        <Link href="/home-nested" push asChild>
+                            <GeneralButton mode="contained" onPressFunction={() => {
+                                // @ts-ignore
+                                router.push("home-nested")
+                            }} text={"Push to nested"} style={{}}/>
+                        </Link>
+                        {canGoBack ? (
+                            <GeneralButton
+                                text={"Back"}
+                                onPressFunction={() => {
+                                    router.back();
+                                }}
+                                mode="contained"
+                                style={{}}
+                            />
+                        ) : null}
+                        <GeneralButton mode="contained" text="Log out!" onPressFunction={authState.logOut}
+                                       style={{backgroundColor: theme.colors.primary}}/>
+                    </View>
+                </View>
+            </ScrollView>
         </AppBackground>
 
 
     );
 }
+
+const makeStyles = (theme) => StyleSheet.create({
+    surface: {
+        height: height * 0.3,
+        width: "100%",
+        borderRadius: 20
+    },
+    image_slider: {
+        flex: 1,
+    },
+    container: {
+        flexGrow: 1,
+        flexDirection: 'column',
+        width: width,
+        alignContent: 'center',
+        paddingBottom: height * 0.05
+    },
+    column_style: {
+        flexWrap: "nowrap",
+        padding: width * 0.03,
+        alignItems: "stretch",
+        gap: 10,
+
+    },
+    card_title: {
+        fontSize: 20,
+        fontWeight: "300",
+    },
+    card_content: {
+        fontSize: 15,
+    },
+    activity_card: {
+        backgroundColor: theme.colors.secondaryContainer,
+    },
+    shop_card: {
+        backgroundColor: theme.colors.tertiaryContainer,
+    }
+});
